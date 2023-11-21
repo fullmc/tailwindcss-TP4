316a5826aa676b0b3252ea2f841379fd97bef1ef
fullmc <lambomarieclaire@yahoo.fr>
2023-11-21 16:14:02 +0100

    Update background image URL in CSS and Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,7 @@ module.exports = {
 				darkbutton: "#282828",
 			},
 			backgroundImage: {
-				banner:
-					"url('https://images.unsplash.com/photo-1624480376260-403eae38dda8?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+				banner: "url('./girl.jpg')",
 			},
 			lineHeight: {
 				12: "3rem",
